0b17fd29ec71ad362e04eaa91ce02e18de48e061
fiza028 <rfarzana70@gmail.com>
2023-03-26 16:56:27 +0600

    changed

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,6 +2,7 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -20,6 +21,46 @@ public class CityList {
             throw new IllegalArgumentException();
         }
         cities.add(city);
+    }
+
+    /**
+     * This deletes a city to the list if that city exists
+     * @param city
+     *      This is to delete the city
+     */
+    public void delete(City city){
+        if(cities.contains(city)==false)
+        {
+            throw new IllegalArgumentException();
+            //when i want to delete city which doesn't exist then it will give exception
+        }
+        cities.remove(city);
+    }
+
+    /**
+     * This returns the total number of cities
+     * @return
+     *      Return the total number of cities
+     */
+    public int Count(){
+        return cities.size();
+    }
+
+    /**
+     * This returns a sorted list of
+     * @return
+     *      Return the sorted list either by cityname or by province name
+     */
+    public List<City> getCities(boolean f){
+        List<City>cityList=cities;
+        Comparator<City>comByCity=(City c1,City c2)->c1.getCityName().compareTo(c2.getCityName());
+        Comparator<City>comByProvince=(City c1, City c2)->c1.getProvinceName().compareTo(c2.getProvinceName());
+        if(f==true)
+            Collections.sort(cityList, comByCity);
+        else
+            Collections.sort(cityList, comByProvince);
+        return cityList;
+
     }
 
     /**
--- app/src/test/java/com/example/citylist/CityListTest.java
+++ app/src/test/java/com/example/citylist/CityListTest.java
@@ -49,4 +49,67 @@ public class CityListTest {
         assertEquals(0, city.compareTo(cityList.getCities().get(0)));
         assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
     }
+
+    @Test
+    public void testDelete(){
+        CityList cityList = new CityList();
+        City city = new City("Khulna", "Khalishpur");
+        City city1 = new City("Dhaka", "Mirpur");
+        cityList.add(city);
+        cityList.add(city1);
+        cityList.delete(city);
+        assertTrue(!cityList.getCities().contains(city));
+    }
+
+    @Test
+    public void testDeleteException(){
+        CityList cityList=new CityList();
+//        City c3 = new City("abc", "xyz");
+        City c1 = new City("Khulna", "Khalishpur");
+        City c2 = new City("Dhaka", "Mirpur");
+        cityList.add(c1);
+        cityList.add(c2);
+        cityList.delete(c1);
+//        cityList.delete(c3);
+        assertThrows(IllegalArgumentException.class, ()->
+        {
+            cityList.delete(c1);
+        });
+    }
+
+    @Test
+    public void testCount(){
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.Count());
+        City c1=new City("ABCD","abcd");
+        cityList.add(c1);
+        assertEquals(2, cityList.Count());
+        cityList.delete(c1);
+        assertEquals(1, cityList.Count());
+
+    }
+
+    @Test
+    public void testSort(){
+//        CityList cityList = mockCityList();
+//        assertEquals(0, mockCity().compareTo(cityList.getCities(true).get(0)));
+//        City cc1=new City("Khulna", "Jashore");
+//        cityList.add(cc1);
+//
+//        assertEquals(0, cc1.compareTo(cityList.getCities(true).get(0)));
+//        assertEquals(0,mockCity().compareTo(cityList.getCities(false).get(1)));
+//
+//        assertEquals(0, cc1.compareTo(cityList.getCities(false).get(1)));
+//        assertEquals(0,mockCity().compareTo(cityList.getCities(true).get(0)));
+
+
+        CityList cityList = new CityList();
+        City cc1=new City("Khulna", "Jashore");
+        City cc2=new City("Dhaka", "Banani");
+
+        cityList.add(cc2);
+        assertEquals(0, cc2.compareTo(cityList.getCities(true).get(0)));
+
+
+    }
 }
